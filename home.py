a12e9aff77ca9953e9e73898a2c0f71f0a0b7790
Magali <mag.dd.enr@gmail.com>
2021-12-22 01:00:52 +0100

    Allegement de la fonction emotion, et divers changements

--- home.py
+++ home.py
@@ -19,9 +19,10 @@ import pickle
 import streamlit as st
 
 def render():
+  #page 
+  distance_slider = st.sidebar.slider("distance threshold", min_value=0.0, max_value=1.0, value=0.6)
   with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
-    st.text('test')
 
   # Import Database pickle 
   path_others ='data/other_member.pkl'
@@ -49,7 +50,7 @@ def render():
     dist_list = []
     # Loop over each encoded face
     for test_encoding in faces_to_find:
-      min_dist = 0.5
+      min_dist = distance_slider
       min_key = "unknown"
       for key in embedded_pics_pickle:
           for encoding in embedded_pics_pickle[key]:
@@ -59,7 +60,7 @@ def render():
                   min_key = key
       actors_list.append(min_key)
       dist_list.append(min_dist)
-    return boxes, actors_list, dist_list, img
+    return boxes, actors_list, dist_list, img 
 
   # convert image from opened file to encoded (embedded) image and face recognition
   def display_image_recognition(image_path):
@@ -68,11 +69,11 @@ def render():
         cv2.rectangle(img, (left, top), (right, bottom), (255, 0, 0), 3)
         font = cv2.FONT_HERSHEY_SIMPLEX
         bottomLeftCornerOfText = (10,500)
-        fontScale  = 2
+        fontScale  = 0.75
         fontColor = (255,0,0)
-        lineType = 4
+        lineType = 2
         # get actor's name above the boxe
-        cv2.putText(img, f'{member}, dist: ({round(min_dist, 2)})', (left, top-10), font, fontScale, fontColor, lineType)
+        cv2.putText(img, f'{member}', (left, top-10), font, fontScale, fontColor, lineType)
     return img
     # Show Image    
     # plt.figure(figsize=(16,10))
@@ -85,11 +86,11 @@ def render():
       cv2.rectangle(img, (left, top), (right, bottom), (255, 0, 0), 3)
       font = cv2.FONT_HERSHEY_SIMPLEX
       bottomLeftCornerOfText = (10,500)
-      fontScale  = 2
+      fontScale  = 0.75
       fontColor = (255,0,0)
-      lineType = 4
+      lineType = 2
       # get actor's emotion under the boxe
-      obj = DeepFace.analyze(img_path = img, actions = ['age', 'gender', 'race', 'emotion'], enforce_detection =False)
+      obj = DeepFace.analyze(img_path = img, actions = ['emotion'], enforce_detection =False) #'age', 'gender', 'race', 
       cv2.putText(img, f"{obj['dominant_emotion']}", (left, bottom+30), font, fontScale, fontColor, lineType)
     return img
     # Show Image    
@@ -97,35 +98,50 @@ def render():
     # plt.axis('off')
     # plt.imshow(img)
 
+  def get_age(image_path):
+    boxes, actors_list, _, img = preprocess_image(image_path)
+    for (top, right, bottom, left), member  in zip(boxes, actors_list):
+      cv2.rectangle(img, (left, top), (right, bottom), (255, 0, 0), 3)
+      font = cv2.FONT_HERSHEY_SIMPLEX
+      bottomLeftCornerOfText = (10,500)
+      fontScale  = 0.75
+      fontColor = (255,0,0)
+      lineType = 2
+      # get actor's emotion under the boxe
+      obj = DeepFace.analyze(img_path = img, actions = ['age'], enforce_detection =False) # 'gender', 'race', 'emotion'
+      cv2.putText(img, f"{obj['dominant_age']}", (left, bottom+30), font, fontScale, fontColor, lineType)
+    return img
+    # Show Image    
+    # plt.figure(figsize=(16,10))
+    # plt.axis('off')
+    # plt.imshow(img)
 
-  # convert face distance to similirity likelyhood
-  def face_distance_to_conf(face_distance, face_match_threshold=0.6):
-      if face_distance > face_match_threshold:
-          range = (1.0 - face_match_threshold)
-          linear_val = (1.0 - face_distance) / (range * 2.0)
-          return linear_val
-      else:
-          range = face_match_threshold
-          linear_val = 1.0 - (face_distance / (range * 2.0))
-          return linear_val + ((1.0 - linear_val) * np.power((linear_val - 0.5) * 2, 0.2))
+  
 
 
-  # convert opencv BRG to regular RGB mode
-  def BGR_to_RGB(image_in_array):
-      return cv2.cvtColor(image_in_array, cv2.COLOR_BGR2RGB)
+  def preprocess_df_filter(image_path):
+    # Get the image
+    img = plt.imread(image_path)
+    # Encode the faces
+    faces_to_find = face_recognition.face_encodings(img)
+    # Get the faces boxes for viz
+    boxes = face_recognition.face_locations(img)#, model = "cnn")
+    actors_list = []
+    dist_list = []
+    # Loop over each encoded face
+    for test_encoding in faces_to_find:
+      min_dist = distance_slider
+      min_key = "unknown"
+      for key in embedded_pics_pickle:
+          for encoding in embedded_pics_pickle[key]:
+              dist = np.linalg.norm(encoding-test_encoding)
+              if dist < min_dist: #erreur dans la correction ici ^^
+                  min_dist = dist
+                  min_key = key
+      actors_list.append(min_key)
+      dist_list.append(min_dist)
+    return actors_list, dist_list
 
-
-
-  # # disable warning signs:
-  # # https://discuss.streamlit.io/t/version-0-64-0-deprecation-warning-for-st-file-uploader-decoding/4465
-  # st.set_option("deprecation.showfileUploaderEncoding", False)
-
-  # # set page config
-  # page_icon =Image.open("/content/drive/MyDrive/Screenema_Streamlit/assets/popcorn.svg")
-  # st.set_page_config(
-  #   page_title="SCREENEMA"
-  #   page_icon=page_icon
-  # )
 
   # title area
   st.markdown("""
@@ -137,14 +153,35 @@ def render():
   screenshot = st.file_uploader(
       label="Select a screenshot from the movie:", type=['jpg']
   )
+
+
   if screenshot:
-    if st.button("recognise"):
-      image = display_image_recognition(screenshot)
-      st.image(image)
-    if st.button("emotion"):
-      image = get_emotion(screenshot)
-      st.image(image)
+    col1, col2, col3 = st.columns([6,1,6])
+    with col1:
+      if st.button("recognise"):
+        image = display_image_recognition(screenshot)
+        st.image(image)
+        # display df with recognised actors preprocess_image
+        actors_filter = preprocess_df_filter(screenshot)
+        df = pd.read_csv('data/TBBT_actors.csv', sep=',')
+        mask = df['actor'].isin(actors_filter[0])
+        df_filtered = df[mask].drop(columns = "Unnamed: 0")
+        df_filtered["distance"] = df_filtered["actor"].apply(lambda x: actors_filter[1][actors_filter[0].index(x)])
+        df_filtered = df_filtered.to_html(escape=False)
+        st.write(df_filtered, unsafe_allow_html=True)
+    with col2:
+      ticket = Image.open('assets/LOGO1.png')
+      st.image(ticket)
+    with col3:
+      if st.button("emotion"):
+        image = get_emotion(screenshot)
+        st.image(image)
+
+      
+
+      
     
+
 
 
     
